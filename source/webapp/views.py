78675503acda87ccc185b8e83953ccdab4cb3ba6
samat_sultanov <samatsayak@gmail.com>
2022-10-28 14:44:10 +0600

    modified form_view.html to change title as per output; created math_result.html to render calculations output; modified script of calculate function in webapp.views to work with post request

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -4,3 +4,33 @@ from django.shortcuts import render
 def calculate(request):
     if request.method == "GET":
         return render(request, "form_view.html")
+    elif request.method == 'POST':
+        print(request.POST)
+        print(request.POST.get('action'))
+        try:
+            num1 = float(request.POST.get('num1'))
+            num2 = float(request.POST.get('num2'))
+            symbol = request.POST.get('action')
+            if symbol == '+':
+                output = num1 + num2
+            elif symbol == '-':
+                output = num1 - num2
+            elif symbol == '/':
+                output = num1 / num2
+            else:
+                output = num1 * num2
+            context = {
+                'output': output,
+                'num1': num1,
+                'num2': num2,
+                'symbol': symbol
+            }
+        except ValueError:
+            context = {
+                'error_message': "Input only numbers and select an action"
+            }
+        except ZeroDivisionError:
+            context = {
+                'error_message': "You can't divide by zero"
+            }
+        return render(request, 'math_result.html', context)
